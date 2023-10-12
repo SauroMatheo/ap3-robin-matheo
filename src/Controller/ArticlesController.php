5ab5504049b33632895761880e7eb89ac649ed34
SauroMatheo <sauromatheo@gmail.com>
2023-10-12 16:48:18 +0200

    Réparé mes bêtises, GET au lieu de route

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -10,43 +10,11 @@ use App\Repository\ArticlesRepository;
 use App\Repository\ImageArticleRepository;
 use App\Repository\StockageRepository;
 
-use App\Entity\Rayons;
-
 class ArticlesController extends AbstractController
 {
-    #[Route('/articles', name: 'app_articles')]
-    public function index(ArticlesRepository $articleRepository, ImageArticleRepository $imageRepository, StockageRepository $stockageRepository): Response
+    #[Route('/articles', name: 'app_tous_articles')]
+    public function tousArticles(ArticlesRepository $articleRepository): Response
     {
-        if (isset($_GET['id'])) {
-            $id = $_GET['id'];
-        
-            $article = $articleRepository->find($id);
-    
-            if ($article == null) {
-                // TODO: Page d'erreur
-                return $this->render('accueil/index.html.twig');
-            }
-    
-            // TODO: Référence = FOURAY123456789012
-    
-            $fou = str_replace(" ", "", $article->getLeFournisseur()->getNom());
-            $ray = str_replace(" ", "", $article->getFkRayons()->getNom());
-            $reference = substr($fou, 0, 3).substr($ray, 0, 3).sprintf("%012d", $id);
-    
-            $stockinternet = $stockageRepository->findByArticle($id);
-            if (count($stockinternet) < 1) {
-                $stockinternet = 0;
-            } else {
-                $stockinternet = $stockinternet[0]->getQuantite();
-            }
-    
-            return $this->render('articles/index.html.twig', [
-                'article' => $article,
-                'reference' => $reference,
-                'stockinternet' => $stockinternet
-            ]);
-        }
-
         $page = 0;
         $max_articles = 4;
 
@@ -59,6 +27,36 @@ class ArticlesController extends AbstractController
         return $this->render('articles/tous_articles.html.twig', [
             'articles' => $articles,
             "page" => $page
+        ]);
+    }
+    
+    #[Route('/articles/{id}', name: 'app_articles')]
+    public function index(int $id, ArticlesRepository $articleRepository, StockageRepository $stockageRepository): Response
+    {
+        $article = $articleRepository->find($id);
+
+        if ($article == null) {
+            // TODO: Page d'erreur
+            return $this->render('accueil/index.html.twig');
+        }
+
+        // TODO: Référence = FOURAY123456789012
+
+        $fou = str_replace(" ", "", $article->getLeFournisseur()->getNom());
+        $ray = str_replace(" ", "", $article->getFkRayons()->getNom());
+        $reference = substr($fou, 0, 3).substr($ray, 0, 3).sprintf("%012d", $id);
+
+        $stockinternet = $stockageRepository->findByArticle($id);
+        if (count($stockinternet) < 1) {
+            $stockinternet = 0;
+        } else {
+            $stockinternet = $stockinternet[0]->getQuantite();
+        }
+
+        return $this->render('articles/index.html.twig', [
+            'article' => $article,
+            'reference' => $reference,
+            'stockinternet' => $stockinternet
         ]);
     }
 }
