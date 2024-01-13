4744169353d3c94ee00cb904b447ca5d1ac609de
SauroMatheo <sauromatheo@gmail.com>
2024-01-13 21:37:54 +0100

    Page panier semi-fonctionnelle

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -13,17 +13,22 @@ class PanierController extends AbstractController
     #[Route('/panier', name: 'app_panier')]
     public function index(ArticlesRepository $articleRepository): Response
     {
-        $panierId = [1, 2];
-        $panierQte = [3, 5];
+        $panierId = [1, 2, 5, 3, 4, 6, 7];
+        $panierQte = [3, 5, 1, 2, 1, 1, 2];
         $articles = [];
+        $total = 0;
 
-        foreach ($panierId as $id) {
-            array_push($articles, $articleRepository->find($id));
+        for ($i=0;$i<count($panierId);$i++) {
+            $article = $articleRepository->find($panierId[$i]);
+            array_push($articles, $article);
+            $total += 1.2 * $article->getPrixUniHT() * $panierQte[$i];
         }
 
 
         return $this->render('panier/panier.php.twig', [
             'articles'=>$articles,
+            'quantites'=>$panierQte,
+            'total'=>$total
         ]);
     }
 }
