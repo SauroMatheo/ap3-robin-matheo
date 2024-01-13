f621a1c424fa401059ca0c5b8cca5d138cc5c801
SauroMatheo <sauromatheo@gmail.com>
2024-01-13 18:59:36 +0100

    Panier contient des éléments

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -6,13 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Repository\ArticlesRepository;
+
 class PanierController extends AbstractController
 {
     #[Route('/panier', name: 'app_panier')]
-    public function index(): Response
+    public function index(ArticlesRepository $articleRepository): Response
     {
+        $panierId = [1, 2];
+        $panierQte = [3, 5];
+        $articles = [];
+
+        foreach ($panierId as $id) {
+            array_push($articles, $articleRepository->find($id));
+        }
+
+
         return $this->render('panier/panier.php.twig', [
-            'controller_name' => 'PanierController',
+            'articles'=>$articles,
         ]);
     }
 }
